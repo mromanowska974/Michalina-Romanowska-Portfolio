09bd44b96f719a11841ea50954a8fe806f8e2519
mromanowska974 <m.romanowska974@gmail.com>
2024-09-17 11:31:16 +0100

    New design for main page
    Main page of the portfolio has been completely redesigned in order to be more practical and UX friendly but it still looks interesting.

--- src/components/main-page/main-page.js
+++ src/components/main-page/main-page.js
@@ -1,29 +1,49 @@
 import React from 'react';
 import styles from './main-page.module.css';
 import '../../fonts/Sacramento-Regular.ttf';
+import { motion } from 'framer-motion';
 
-const MainPage = () => (
-  <div className={styles.container}>
-    <div className={styles.introduction}>
-      <p>Michalina Romanowska</p>
-    </div>
-    <div className={styles.content}>
-      <div className={styles.imageWrap}></div>
-      <div className={styles.frontUp}>
-        <p>Michalina</p>
+const MainPage = () => {
+  const initial = {
+    opacity: 0
+  }
+
+  const animate = {
+    opacity: 1
+  }
+
+  const transition = {
+    ease: "easeOut", 
+    duration: 2
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.navbar}>
+        <div className={styles.logo}>
+          <p>MR</p>
+        </div>
+        <div className={styles.btnBox}>
+          <button style={{'--i': '0s'}}>About Me</button>
+          <button style={{'--i': '1s'}}>Projects</button>
+          <button style={{'--i': '2s'}}>CV</button>
+          <button style={{'--i': '3s'}}>Contact</button>
+        </div>
       </div>
-      <div className={styles.frontDown}>
-        <p>Romanowska</p>
-      </div>
-      <div className={styles.frame}></div>
+      <motion.div 
+        className={styles.content}
+        initial={initial}
+        animate={animate}
+        transition={transition}
+      >
+        <div className={styles.imageWrap}></div>
+        <div className={styles.introduction}>
+          <h1>Michalina Romanowska</h1>
+          <p>Junior Web Developer with passion, curiosity and readiness to gain new experience.</p>
+        </div>
+      </motion.div>
     </div>
-    <div className={styles.btnBox}>
-      <button style={{'--i': '0s'}}>About Me</button>
-      <button style={{'--i': '1s'}}>Projects</button>
-      <button style={{'--i': '2s'}}>CV</button>
-      <button style={{'--i': '3s'}}>Contact</button>
-    </div>
-  </div>
-);
+  );
+}
 
 export default MainPage;
